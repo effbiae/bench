57057c949ba73627a4a06428d6a03b0470fe2b82
effbiae <effbiae@gmail.com>
2026-09-22 13:11:48 +1000

    bigfact

--- s/bigfact/bigfact.py
+++ s/bigfact/bigfact.py
@@ -1,11 +1,11 @@
-#print (x!+x)!/x!
+#print (x!+x)!/x!!
 import sys;sys.set_int_max_str_digits(1000000)
-def prod(x,y): #(f+x)!/f!
+def prod(x,y): #(x+y)!/x!
  r=x+1
  for i in range(r+1,x+y+1):r*=i
  return r
 fact=lambda x:prod(0,x)
-exp= lambda x:fact(fact(x)+x)/fact(fact(x)) #(x!+x)!/x!
-expO=lambda x:prod(fact(x),x) #simplified (x!+x)!/x!
-a=5;assert(expO(a)==exp(a))
-print(expO(int((sys.argv+["300"])[1])))
+f =lambda x:fact(fact(x)+x)/fact(fact(x)) #(x!+x)!/x!!
+fO=lambda x:prod(fact(x),x) #optimised f
+a=5;assert(fO(a)==f(a))
+print(fO(int((sys.argv+["300"])[1])))
