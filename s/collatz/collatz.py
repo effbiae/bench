61dc578bc071ef01573656f85f2fc093fe78296b
effbiae <effbiae@gmail.com>
2026-09-23 16:16:40 +1000

    delete bigfact, collatz set at 13

--- s/collatz/collatz.py
+++ s/collatz/collatz.py
@@ -1,24 +1,12 @@
 #find longest collatz sequence for a range of integers
 import math,sys
-cache={}
-def encache(trajectory,remaining_steps):
- for i,start in enumerate(trajectory):
-  cache[start]=len(trajectory)-i+remaining_steps
-def collatz(start):
- steps=0
- number=start
- trajectory=[start]
- while number!=1:
-  if number in cache:
-   encache(trajectory,cache[number])
-   return cache[start]
-  if number%2==0:number//= 2
-  else:number=3*number+1
-  trajectory.append(number)
-  steps+=1
- encache(trajectory,0)
- return steps
-x=int((sys.argv+["400"])[1])
-s=2**x;n=x**2;l=0
-for start in range(s,s+n):l=max(l,collatz(start))
-print(l)
+def collatz(x):
+ i=0
+ while x!=1:
+  if x%2==0:x//= 2
+  else:x=3*x+1
+  i+=1
+ return i
+x=int((sys.argv+["10000"])[1])
+s=1+2**x
+print(collatz(s))
