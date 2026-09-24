c0f2868dc427940472d1c98cd3cd931f952520ac
effbiae <effbiae@gmail.com>
2026-09-24 22:08:03 +1000

    slower spectralnorm.py

--- s/spectralnorm/spectralnorm.py
+++ s/spectralnorm/spectralnorm.py
@@ -1,66 +1,39 @@
 # The Computer Language Benchmarks Game
-# https://salsa.debian.org/benchmarksgame-team/benchmarksgame/
+#   https://salsa.debian.org/benchmarksgame-team/benchmarksgame/
 #
-# Contributed by Sebastien Loisel
-# Fixed by Isaac Gouy
-# Sped up by Josh Goldfoot
-# Dirtily sped up by Simon Descarpentries
-# Used list comprehension by Vadim Zelenin
-# Concurrency by Jason Stitt
-# Concurrency simplified by Matt Vollrath
-# Optimized math by Adam Beckmeyer
+#   Naive transliteration from Sebastien Loisel's C program
+#   contributed by Isaac Gouy
+#
 
-
-from itertools import repeat
 from math import sqrt
-from multiprocessing import Pool
-from sys import argv
-
+import sys
 
 def eval_A(i, j):
-    ij = i + j
-    return ij * (ij + 1) // 2 + i + 1
+  return 1.0/((i+j)*(i+j+1)/2+i+1)
 
+def eval_A_times_u(N, u, Au):
+  for i in range(N):  
+    Au[i]=0
+    for j in range(N): Au[i]+=eval_A(i,j)*u[j]      
 
-def A_sum(u, i):
-    return sum(u_j / eval_A(i, j) for j, u_j in enumerate(u))
+def eval_At_times_u(N, u, Au):
+  for i in range(N):  
+    Au[i]=0
+    for j in range(N): Au[i]+=eval_A(j,i)*u[j]   
 
+def eval_AtA_times_u(N, u, AtAu):
+  v=[0]*N; eval_A_times_u(N,u,v); eval_At_times_u(N,v,AtAu)
 
-def At_sum(u, i):
-    return sum(u_j / eval_A(j, i) for j, u_j in enumerate(u))
-
-
-def multiply_AtAv(u):
-    r = range(len(u))
-
-    tmp = pool.starmap(
-        A_sum,
-        zip(repeat(u), r)
-    )
-    return pool.starmap(
-        At_sum,
-        zip(repeat(tmp), r)
-    )
-
-
-def main():
-    n = int((argv+["1024"])[1])
-    u = [1] * n
-
-    for _ in range(10):
-        v = multiply_AtAv(u)
-        u = multiply_AtAv(v)
-
-    vBv = vv = 0
-
-    for ue, ve in zip(u, v):
-        vBv += ue * ve
-        vv  += ve * ve
-
-    result = sqrt(vBv/vv)
-    print("{0:.9f}".format(result))
-
-
+def main(n):
+  u=[1]*n
+  v=[0]*n
+  for i in range(10):
+    eval_AtA_times_u(n,u,v)
+    eval_AtA_times_u(n,v,u)
+  vBv=vv=0
+  for i in range(n): vBv+=u[i]*v[i]; vv+=v[i]*v[i]  
+  print("%.9f" % sqrt(vBv/vv))
+  
 if __name__ == '__main__':
-    with Pool(processes=4) as pool:
-        main()
+  main( int(sys.argv[1]) if len(sys.argv) > 1 else 100 )  
+
